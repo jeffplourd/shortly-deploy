3c2aad63cac2bb2c0693bbe386e7049ee9c48468
Hack Reactor Students <accounts@hackreactor.com>
2015-07-29 15:11:25 -0700

    Normalizing port

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+// add processing function
+var port = normalizePort(process.env.PORT || '4568');
 
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
+
+var normalizePort = function(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  } 
+  return false;
+}
